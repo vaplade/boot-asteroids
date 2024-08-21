56650218dc5d053d15cd353886698b560385ce0b
vaplade <makeshifth2h@gmail.com>
2024-08-21 01:22:04 -0700

    Add draw method to Player class and insert it into main function

--- player.py
+++ player.py
@@ -1,14 +1,15 @@
 import pygame
+from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__()
+        super().__init__(x, y, None)
         self.x = x
         self.y = y
-        position = pygame.Vector2(self.x, self.y)
-        rotation = 0
-        # in the player class
+        self.radius = PLAYER_RADIUS
+        self.position = pygame.Vector2(self.x, self.y)
+        self.rotation = 0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -16,3 +17,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    def draw(self, screen):
+        return pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
+        
+
